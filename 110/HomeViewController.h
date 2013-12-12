323e2f6d52e420461339308187a8b1959757fa5e
a <first@shinsenkunpengmatoiMac-196.local>
2013-12-12 17:42:04 +0800

    aa

---
+++ 110/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  110
+//
+//  Created by a on 13-12-12.
+//  Copyright (c) 2013年 ZZJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ 110/HomeViewController.m
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.m
+//  110
+//
+//  Created by a on 13-12-12.
+//  Copyright (c) 2013年 ZZJL. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(void)sb
+{
+    NSLog(@"nishisb");
+}
+@end
